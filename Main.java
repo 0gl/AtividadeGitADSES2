3a63afc33c46f2caa1f86abf43a4ee83fab746f5
0gl <58913476+0gl@users.noreply.github.com>
2021-05-04 21:41:55 -0300

    Início
    Inserção das classes.

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main() {
+
+        //apresentar o programa
+
+        //oferecer as opcoes de circulo ou quadrado
+
+        //uma classe de calculos pra cada
+
+        //receber e verificar os valores (reais, pois circunferencia envolve π)
+
+        //usar objetos das classes para atribuir-lhes valores e retornar os calculos
+
+        //perguntar se deseja fazer de novo
+
+        //encerrar o programa
+
+    }
+}
